3dfd4826e7173a0b0c86f13ffa3f8c8a3490c735
Gogs <gogs@fake.local>
2023-11-17 13:28:32 +0200

    Group1

---
+++ Lab4/GroupWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Lab4
+{
+    /// <summary>
+    /// Логика взаимодействия для GroupWindow.xaml
+    /// </summary>
+    public partial class GroupWindow : Window
+    {
+        private HttpClient client;
+        private Group? group;
+        public GroupWindow()
+        {
+            InitializeComponent();
+            client = new HttpClient();
+            Task.Run(() => Load());
+        }
+        private async Task Load()
+        {
+            List<Group>? list = await client.GetFromJsonAsync<List<Group>>("http://localhost:5201/api/groups");
+            Dispatcher.Invoke(() =>
+            {
+                ListGroups.ItemsSource = null;
+                ListGroups.Items.Clear();
+                ListGroups.ItemsSource = list;
+            });
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
